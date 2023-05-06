156eca7f51d74bcf2ab546750ab77e345b67eac2
Evgeni Sautin <evgeni.sautin@gmail.com>
2023-05-06 18:31:52 +0300

    Make tests async

--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -1,10 +1,18 @@
-from fastapi.testclient import TestClient
+import pytest
+from httpx import AsyncClient
 
 from app.main import app
 
-client = TestClient(app)
+
+@pytest.mark.asyncio
+async def test_non_existent_path_returns_404_status_code():
+    async with AsyncClient(app=app, base_url="http://localhost:8000") as ac:
+        response = await ac.get("/non-existent")
+    assert response.status_code == 404
 
 
-def test_graphql_path_works():
-    response = client.get("/graphql")
+@pytest.mark.asyncio
+async def test_graphql_path_returns_200_status_code():
+    async with AsyncClient(app=app, base_url="http://localhost:8000") as ac:
+        response = await ac.get("/graphql")
     assert response.status_code == 200
