e0a7d67656f0318fbb2f961665f20ec8619078e8
Evgeni Sautin <evgeni.sautin@gmail.com>
2023-05-13 15:39:32 +0300

    Refactor es utils after changing the client

--- app/search_engine/es_utils.py
+++ app/search_engine/es_utils.py
@@ -28,7 +28,7 @@ async def es_create_index(index_name: str):
     Args:
         index_name (str): The name of the index to create.
     """
-    async with get_es() as es:
+    async for es in get_es():
         await es.indices.create(index=index_name)
 
 
@@ -42,7 +42,7 @@ async def es_index_document(index_name: str, doc_id: str, doc):
         doc: The document to index.
     """
     prepared_doc = _prepare_mongo_doc_for_es(doc)
-    async with get_es() as es:
+    async for es in get_es():
         await es.index(index=index_name, id=doc_id, document=prepared_doc)
 
 
@@ -56,8 +56,8 @@ async def es_update_document(index_name: str, doc_id: str, doc):
         doc: The updated document.
     """
     prepared_doc = _prepare_mongo_doc_for_es(doc)
-    async with get_es() as es:
-        await es.update(index=index_name, id=doc_id, body={"doc": prepared_doc})
+    async for es in get_es():
+        await es.update(index=index_name, id=doc_id, doc=prepared_doc)
 
 
 async def es_delete_document(index_name: str, doc_id: str):
@@ -68,7 +68,7 @@ async def es_delete_document(index_name: str, doc_id: str):
         index_name (str): The name of the index where the document is located.
         doc_id (str): The ID of the document to delete.
     """
-    async with get_es() as es:
+    async for es in get_es():
         await es.delete(index=index_name, id=doc_id)
 
 
@@ -90,7 +90,7 @@ async def es_search(index_name: str, search_text: str, page: int, per_page: int)
     if per_page < 1:
         raise GraphQLError(message="Per page must be greater than or equal to 1.")
 
-    async with get_es() as es:
+    async for es in get_es():
         return await es.search(
             index=index_name,
             query={"bool": {"must": {"query_string": {"query": search_text}}}},
