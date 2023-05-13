363f9ae68fbffa840859b11305ae341b45adbf47
Evgeni Sautin <evgeni.sautin@gmail.com>
2023-05-13 15:34:48 +0300

    Use normal coroutine instead of asynccontextmanager

--- app/search_engine/elasticsearch.py
+++ app/search_engine/elasticsearch.py
@@ -1,5 +1,3 @@
-from contextlib import asynccontextmanager
-
 from elasticsearch import AsyncElasticsearch
 
 from ..config import get_settings
@@ -7,7 +5,6 @@ from ..config import get_settings
 settings = get_settings()
 
 
-@asynccontextmanager
 async def get_es() -> AsyncElasticsearch:
     async with AsyncElasticsearch(hosts=[settings.elasticsearch_url]) as client:
         yield client
